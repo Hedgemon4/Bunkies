b730f2f162710c5fa98b4f4110bc9baf467ddb2c
Hedgemon4 <sethlinc4@gmail.com>
2022-12-01 18:51:23 -0800

    Started changing time input

--- app/src/main/java/com/example/bunkies/EventEditActivity.java
+++ app/src/main/java/com/example/bunkies/EventEditActivity.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.sql.Time;
 import java.time.LocalTime;
 
 public class EventEditActivity extends AppCompatActivity
@@ -17,6 +18,9 @@ public class EventEditActivity extends AppCompatActivity
     private TextView eventDateTV, eventTimeTV;
 
     private LocalTime time;
+
+    private EditText hours;
+    private EditText minutes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -27,6 +31,9 @@ public class EventEditActivity extends AppCompatActivity
         time = LocalTime.now();
         eventDateTV.setText("Date: " + CalendarUtils.formattedDate(CalendarUtils.selectedDate));
         eventTimeTV.setText("Time: " + CalendarUtils.formattedTime(time));
+
+        hours = findViewById(R.id.editEventHours);
+        minutes = findViewById(R.id.editEventMinutes);
 
         getSupportActionBar().setTitle("Calendar");
         getSupportActionBar().setBackgroundDrawable(new ColorDrawable(Color.parseColor("#FF3700B3")));
@@ -44,6 +51,7 @@ public class EventEditActivity extends AppCompatActivity
 
     public void saveEventAction(View view)
     {
+        time = LocalTime.of(Integer.parseInt(hours.getText().toString()), Integer.parseInt(minutes.getText().toString()));
         String eventName = eventNameET.getText().toString();
         Event newEvent = new Event(eventName, CalendarUtils.selectedDate, time);
         Event.eventsList.add(newEvent);
